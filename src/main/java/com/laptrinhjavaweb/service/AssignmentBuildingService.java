77815655271a11d444adff01d18fa4e2c39140c0
Your Name <you@example.com>
2022-11-17 14:19:14 +0700

    update

--- src/main/java/com/laptrinhjavaweb/service/AssignmentBuildingService.java
+++
@@ -1,8 +0,0 @@
-package com.laptrinhjavaweb.service;
-
-import com.laptrinhjavaweb.dto.AssignmentBuildingDTO;
-
-
-public interface AssignmentBuildingService {
-    AssignmentBuildingDTO save(AssignmentBuildingDTO assignmentBuildingDTO);
-}
