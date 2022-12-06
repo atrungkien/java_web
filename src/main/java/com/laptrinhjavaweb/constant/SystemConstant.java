efb2603a8722501f3a724ef100747b797ba39ae4
Your Name <you@example.com>
2022-12-06 23:45:21 +0700

    update

--- src/main/java/com/laptrinhjavaweb/constant/SystemConstant.java
+++ src/main/java/com/laptrinhjavaweb/constant/SystemConstant.java
@@ -5,6 +5,8 @@ public class SystemConstant {
     public static final String ADMIN_ROLE = "ROLE_ADMIN";
     /*Spring security 4: ROLE_ADMIN, Spring security 3 not required*/
     public static final String USER_ROLE = "ROLE_USER";
+    public static final String MANAGER = "ROLE_USER";
+    public static final String STAFF = "ROLE_USER";
     public static final String MANAGER_ROLE = "MANAGER";
     public static final String HOME = "/trang-chu";
     public static final String ADMIN_HOME = "/admin/home";
